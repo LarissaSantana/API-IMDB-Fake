ff417bd5c1913dc1e8c11f246866eaf3fc5e4d2e
Larissa Santana <lariissasantana_n@hotmail.com>
2020-08-28 21:13:43 -0300

    Criando a BaseEntity<T>.

---
+++ IMDb/3 - Domain/IMDb.Domain/DomainObjects/BaseEntity.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IMDb.Domain.DomainObjects
+{
+    public abstract class BaseEntity<T> : AbstractValidator<T> where T : BaseEntity<T>
+    {
+        public Guid Id { get; protected set; }
+
+        [NotMapped]
+        public ValidationResult ValidationResult { get; protected set; }
+
+        protected BaseEntity()
+        {
+            Id = Guid.NewGuid();
+            ValidationResult = new ValidationResult();
+        }
+        public abstract bool IsValid();
+    }
+}
