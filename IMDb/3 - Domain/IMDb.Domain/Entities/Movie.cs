5707e6b82f3253b33c2dd8d2869ab4335117d285
Larissa Santana <lariissasantana_n@hotmail.com>
2020-08-28 21:20:43 -0300

    Criando a Entidade Movie e o Enum Genre.

---
+++ IMDb/3 - Domain/IMDb.Domain/Entities/Movie.cs
@@ -0,0 +1,30 @@
+﻿using IMDb.Domain.DomainObjects;
+using IMDb.Domain.Enums;
+using System;
+
+namespace IMDb.Domain.Entities
+{
+    public class Movie : BaseEntity<Movie>
+    {
+        public string Title { get; private set; }
+        public Genre Genre { get; private set; }
+        public float? Mean { get; private set; }
+
+        protected Movie()
+        {
+
+        }
+
+        public Movie(Guid id, string title, Genre genre)
+        {
+            Id = id;
+            Title = title;
+            Genre = genre;
+        }
+
+        public override bool IsValid()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IMDb/3 - Domain/IMDb.Domain/Enums/Genre.cs
@@ -0,0 +1,11 @@
+﻿namespace IMDb.Domain.Enums
+{
+    public enum Genre : byte
+    {
+        SciFi = 1,
+        Action = 2,
+        Animation = 3,
+        Fantasy = 4,
+        Drama = 5
+    }
+}
