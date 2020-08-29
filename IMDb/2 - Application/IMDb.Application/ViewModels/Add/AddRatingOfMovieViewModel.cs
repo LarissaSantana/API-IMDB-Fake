5146d8d420cd39a856b036e1fae98435f4262537
Larissa Santana <lariissasantana_n@hotmail.com>
2020-08-29 12:00:29 -0300

    Criando AddRatingOfMovieViewModel.

---
+++ IMDb/2 - Application/IMDb.Application/ViewModels/Add/AddRatingOfMovieViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace IMDb.Application.ViewModels.Add
+{
+    public class AddRatingOfMovieViewModel
+    {
+        [Required(ErrorMessage = "The Rate is Required")]
+        [Range(0, 4, ErrorMessage = "O Rate deve estar entre 0 e 4!")]
+        public int Rate { get; set; }
+
+        [Required(ErrorMessage = "The MovieId is Required")]
+        public Guid MovieId { get; set; }
+
+        public Guid UserId { get; set; }
+    }
+}
