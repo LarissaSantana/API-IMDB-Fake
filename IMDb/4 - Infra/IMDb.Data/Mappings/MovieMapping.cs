5e7557500d260283d8700193e9884fe83591922d
Larissa Santana <lariissasantana_n@hotmail.com>
2020-08-28 21:51:13 -0300

    Criando o MovieMapping.

---
+++ IMDb/4 - Infra/IMDb.Data/Mappings/MovieMapping.cs
@@ -0,0 +1,29 @@
+﻿using IMDb.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IMDb.Data.Mappings
+{
+    public class MovieMapping : IEntityTypeConfiguration<Movie>
+    {
+        public void Configure(EntityTypeBuilder<Movie> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .HasColumnName("MovieId");
+
+            builder.Property(x => x.Genre)
+                .IsRequired();
+
+            builder.Property(x => x.Title)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.ToTable("Movie", "IMDb");
+
+            builder.Ignore(x => x.ValidationResult);
+            builder.Ignore(x => x.CascadeMode);
+        }
+    }
+}
