6a2fb0c7d9b0430ff6ca6251ecc2e6fc4bd0468b
Larissa Santana <lariissasantana_n@hotmail.com>
2020-08-28 22:13:01 -0300

    Criando o RoleMapping

---
+++ IMDb/4 - Infra/IMDb.Data/Mappings/RoleMapping.cs
@@ -0,0 +1,26 @@
+﻿using IMDb.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IMDb.Data.Mappings
+{
+    public class RoleMapping : IEntityTypeConfiguration<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .HasColumnName("RoleId");
+
+            builder.Property(x => x.Name)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.ToTable("Role", "IMDb");
+
+            builder.Ignore(x => x.ValidationResult);
+            builder.Ignore(x => x.CascadeMode);
+        }
+    }
+}
