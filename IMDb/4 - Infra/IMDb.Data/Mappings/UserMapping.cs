cae346d3a23c8960a5516efd093ba8d509c8a535
Larissa Santana <lariissasantana_n@hotmail.com>
2020-08-28 22:10:37 -0300

    Criando o UserMapping

---
+++ IMDb/4 - Infra/IMDb.Data/Mappings/UserMapping.cs
@@ -0,0 +1,37 @@
+﻿using IMDb.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IMDb.Data.Mappings
+{
+    public class UserMapping : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .HasColumnName("UserId");
+
+            builder.Property(x => x.Name)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.HasOne(x => x.Role)
+                .WithMany(u => u.Users)
+                .HasForeignKey(x => x.RoleId);
+
+            builder.Property(x => x.Password)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.Property(x => x.Status)
+                .IsRequired();
+
+            builder.ToTable("User", "IMDb");
+
+            builder.Ignore(x => x.ValidationResult);
+            builder.Ignore(x => x.CascadeMode);
+        }
+    }
+}
