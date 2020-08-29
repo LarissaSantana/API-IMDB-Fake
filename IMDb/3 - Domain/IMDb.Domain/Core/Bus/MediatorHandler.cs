dfd9d480a9074cedc18f679ec16954d483497778
Larissa Santana <lariissasantana_n@hotmail.com>
2020-08-29 00:16:40 -0300

    Implementação do MediatorHandler.

---
+++ IMDb/3 - Domain/IMDb.Domain/Core/Bus/MediatorHandler.cs
@@ -0,0 +1,21 @@
+﻿using IMDb.Domain.Core.Messages;
+using MediatR;
+using System.Threading.Tasks;
+
+namespace IMDb.Domain.Core.Bus
+{
+    public class MediatorHandler : IMediatorHandler
+    {
+        private readonly IMediator _mediator;
+
+        public MediatorHandler(IMediator mediatr)
+        {
+            _mediator = mediatr;
+        }
+
+        public Task<bool> SendCommand<T>(T command) where T : Command
+        {
+            return _mediator.Send(command);
+        }
+    }
+}
