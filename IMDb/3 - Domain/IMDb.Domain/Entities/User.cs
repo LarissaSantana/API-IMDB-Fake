938fd44e5ecaebd3232b42b4db15524d541abe8d
Larissa Santana <lariissasantana_n@hotmail.com>
2020-08-28 21:33:12 -0300

    Criando a entidade User.

---
+++ IMDb/3 - Domain/IMDb.Domain/Entities/User.cs
@@ -0,0 +1,28 @@
+﻿using IMDb.Domain.DomainObjects;
+using System;
+
+namespace IMDb.Domain.Entities
+{
+    public class User : BaseEntity<User>
+    {
+        public string Name { get; private set; }
+        public bool Status { get; private set; }
+        public string Password { get; private set; }
+
+        protected User() { }
+
+        public User(Guid id, string name, bool status, string password)
+        {
+            Id = id;
+            Name = name;
+            Status = status;
+            Password = password;
+        }
+
+        public override bool IsValid()
+        {
+            ValidationResult = Validate(this);
+            return ValidationResult.IsValid;
+        }
+    }
+}
