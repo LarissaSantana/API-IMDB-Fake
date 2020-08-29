4f4c771d69a672e10ce67a0966355741938fef00
Larissa Santana <lariissasantana_n@hotmail.com>
2020-08-29 11:16:45 -0300

    Criando estrutura para DomainNotifications. (parte 2)

---
+++ IMDb/3 - Domain/IMDb.Domain/Core/Notifications/IDomainNotificationHandler.cs
@@ -0,0 +1,15 @@
+﻿using IMDb.Domain.Core.Messages;
+using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IMDb.Domain.Core.Notifications
+{
+    public interface IDomainNotificationHandler<T> : INotificationHandler<T> where T : Event
+    {
+        bool HasNotifications();
+        List<T> GetNotifications();
+        Task Handle(DomainNotification message, CancellationToken cancellationToken);
+    }
+}
