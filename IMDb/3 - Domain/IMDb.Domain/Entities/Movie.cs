9e2dbb17a8e5ff89e56893f54ed5e76105a30d85
Larissa Santana <lariissasantana_n@hotmail.com>
2020-08-28 21:40:30 -0300

    Criando a entidade RatingOfMovie e relacionando com Movie e adicionando o relaciomento de CastOfMovie.

--- IMDb/3 - Domain/IMDb.Domain/Entities/Movie.cs
+++ IMDb/3 - Domain/IMDb.Domain/Entities/Movie.cs
@@ -1,6 +1,7 @@
 ﻿using IMDb.Domain.DomainObjects;
 using IMDb.Domain.Enums;
 using System;
+using System.Collections.Generic;
 
 namespace IMDb.Domain.Entities
 {
@@ -9,10 +10,13 @@ namespace IMDb.Domain.Entities
         public string Title { get; private set; }
         public Genre Genre { get; private set; }
         public float? Mean { get; private set; }
+        public virtual ICollection<CastOfMovie> CastOfMovies { get; private set; }
+        public virtual ICollection<RatingOfMovie> MovieRatings { get; private set; }
 
         protected Movie()
         {
-
+            CastOfMovies = new List<CastOfMovie>();
+            MovieRatings = new List<RatingOfMovie>();
         }
 
         public Movie(Guid id, string title, Genre genre)
---
+++ IMDb/3 - Domain/IMDb.Domain/Entities/RatingOfMovie.cs
@@ -0,0 +1,28 @@
+﻿using IMDb.Domain.DomainObjects;
+using System;
+
+namespace IMDb.Domain.Entities
+{
+    public class RatingOfMovie : BaseEntity<RatingOfMovie>
+    {
+        public int Rate { get; private set; }
+        public Guid UserId { get; private set; }
+        public virtual User User { get; private set; }
+        public Guid MovieId { get; private set; }
+        public virtual Movie Movie { get; private set; }
+        public RatingOfMovie() { }
+
+        public RatingOfMovie(Guid id, Guid userId, Guid movieId)
+        {
+            Id = id;
+            UserId = userId;
+            MovieId = movieId;
+        }
+
+        public override bool IsValid()
+        {
+            ValidationResult = Validate(this);
+            return ValidationResult.IsValid;
+        }
+    }
+}
