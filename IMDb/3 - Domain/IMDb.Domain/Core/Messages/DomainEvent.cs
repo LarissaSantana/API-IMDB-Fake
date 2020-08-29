92a469bff1e541be78a91014016bdc88bed15680
Larissa Santana <lariissasantana_n@hotmail.com>
2020-08-29 11:12:35 -0300

    Criando estrutura para DomainNotifications.

---
+++ IMDb/3 - Domain/IMDb.Domain/Core/Messages/DomainEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IMDb.Domain.Core.Messages
+{
+    public class DomainEvent : Event
+    {
+        public DomainEvent(Guid entityId)
+        {
+            EntityId = entityId;
+        }
+    }
+}
