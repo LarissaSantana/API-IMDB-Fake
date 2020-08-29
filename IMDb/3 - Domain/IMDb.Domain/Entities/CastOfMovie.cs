3519f9fa14b327394a1fd4627f6026f013726378
Larissa Santana <lariissasantana_n@hotmail.com>
2020-08-28 21:29:52 -0300

    Criando a entidade CastOfMovie.

---
+++ IMDb/3 - Domain/IMDb.Domain/Entities/CastOfMovie.cs
@@ -0,0 +1,27 @@
+﻿using IMDb.Domain.DomainObjects;
+using System;
+
+namespace IMDb.Domain.Entities
+{
+    public class CastOfMovie : BaseEntity<CastOfMovie>
+    {
+        public Guid MovieId { get; private set; }
+        public virtual Movie Movie { get; private set; }
+        public Guid CastId { get; private set; }
+        public virtual Cast Cast { get; private set; }
+
+        protected CastOfMovie() { }
+
+        public CastOfMovie(Guid id, Guid movieId, Guid castId)
+        {
+            Id = id;
+            MovieId = id;
+            CastId = id;
+        }
+
+        public override bool IsValid()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
