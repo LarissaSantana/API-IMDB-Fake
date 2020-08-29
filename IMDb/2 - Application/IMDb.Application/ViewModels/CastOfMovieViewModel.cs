ed22ff30becc7348cff42a77508369f4189873ad
Larissa Santana <lariissasantana_n@hotmail.com>
2020-08-29 12:28:18 -0300

    Criando viewModels de Movie e CastOfMovie para retorno.

---
+++ IMDb/2 - Application/IMDb.Application/ViewModels/CastOfMovieViewModel.cs
@@ -0,0 +1,12 @@
+﻿using IMDb.Domain.Enums;
+using System;
+
+namespace IMDb.Application.ViewModels
+{
+    public class CastOfMovieViewModel
+    {
+        public Guid CastOfMovieId { get; set; }
+        public string Name { get; set; }
+        public CastType CastType { get; set; }
+    }
+}
---
+++ IMDb/2 - Application/IMDb.Application/ViewModels/MovieViewModel.cs
@@ -0,0 +1,16 @@
+﻿using IMDb.Domain.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace IMDb.Application.ViewModels
+{
+    public class MovieViewModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public Genre Genre { get; set; }
+        public float? Mean { get; set; }
+        public ICollection<CastOfMovieViewModel> CastOfMovieList { get; set; }
+        //public ICollection<RatingOfMovie> MyProperty { get; set; }
+    }
+}
