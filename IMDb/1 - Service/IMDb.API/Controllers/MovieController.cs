cec38767c1e81b084ec5460ca4977a4b0ed2d653
Larissa Santana <lariissasantana_n@hotmail.com>
2020-08-30 15:58:42 -0300

    Adicionando as autorizações nos endpoints de Movie.

--- IMDb/1 - Service/IMDb.API/Controllers/MovieController.cs
+++ IMDb/1 - Service/IMDb.API/Controllers/MovieController.cs
@@ -2,6 +2,7 @@
 using IMDb.Application.ViewModels.Movie;
 using IMDb.Application.ViewModels.Movie.Add;
 using IMDb.Domain.Core.Notifications;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Linq;
@@ -9,6 +10,7 @@ using System.Linq;
 namespace IMDb.API.Controllers
 {
     //TODO: fazer versionamento
+    [Authorize]
     [Route("api/movie")]
     public class MovieController : BaseController
     {
@@ -21,6 +23,7 @@ namespace IMDb.API.Controllers
         }
 
         [HttpPost]
+        [Authorize(Roles = "e33a5da4-4c46-4f0e-8ef7-8d01a12f9884")]
         public IActionResult AddMovie([FromBody] AddMovieViewModel viewModel)
         {
             var errors = GetErrorListFromModelState();
@@ -31,20 +34,16 @@ namespace IMDb.API.Controllers
             return GetResponse();
         }
 
-        [HttpPost]
-        [Route("movieRating")]
-        public IActionResult AddRatingOfMovie([FromBody] AddRatingOfMovieViewModel viewModel)
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult teste()
         {
-            var errors = GetErrorListFromModelState();
-            if (errors.Any())
-                return BadRequest(errors);
 
-            _movieAppService.AddRatingOfMovie(viewModel);
-            return GetResponse();
         }
 
         [HttpPost]
         [Route("cast")]
+        [Authorize(Roles = "e33a5da4-4c46-4f0e-8ef7-8d01a12f9884")]
         public IActionResult AddCast([FromBody] AddCastViewModel viewModel)
         {
             var errors = GetErrorListFromModelState();
@@ -55,8 +54,22 @@ namespace IMDb.API.Controllers
             return GetResponse();
         }
 
+        [HttpPost]
+        [Route("ratingOfMovie")]
+        [Authorize(Roles = "e33a5da4-4c46-4f0e-8ef7-8d01a12f9884")]
+        public IActionResult AddRatingOfMovie([FromBody] AddRatingOfMovieViewModel viewModel)
+        {
+            var errors = GetErrorListFromModelState();
+            if (errors.Any())
+                return BadRequest(errors);
+
+            _movieAppService.AddRatingOfMovie(viewModel);
+            return GetResponse();
+        }
+
         [HttpGet]
         [Route("{id:Guid}")]
+        [AllowAnonymous]
         public IActionResult GetMovieById(Guid id)
         {
             var movie = _movieAppService.GetMovieById(id);
@@ -65,6 +78,7 @@ namespace IMDb.API.Controllers
 
         [HttpGet]
         [Route("moviesByFilters")]
+        [AllowAnonymous]
         public IActionResult GetMovieByFilters([FromBody] MovieFilterViewModel viewModel, int pageNumber = 1, int pageSize = 10)
         {
             var movies = _movieAppService.GetMovies(viewModel, pageNumber, pageSize);
