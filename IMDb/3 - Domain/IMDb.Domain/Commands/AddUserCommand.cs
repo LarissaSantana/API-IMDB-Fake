9c52c2293cc0473f4da3cc6458a10746ac70cfed
Larissa Santana <lariissasantana_n@hotmail.com>
2020-08-29 23:33:07 -0300

    Criando command para inserir usuário.

---
+++ IMDb/3 - Domain/IMDb.Domain/Commands/AddUserCommand.cs
@@ -0,0 +1,19 @@
+﻿using IMDb.Domain.Core.Messages;
+using System;
+
+namespace IMDb.Domain.Commands
+{
+    public class AddUserCommand : Command
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public Guid RoleId { get; set; }
+
+        public AddUserCommand(string name, string password, Guid roleId)
+        {
+            Name = name;
+            Password = password;
+            RoleId = roleId;
+        }
+    }
+}
